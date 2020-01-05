bc1e8d8be1b816c4da2e8aeab5bf1ebd80f6515f
DarkPhoenix6853 <caleb.klein@hotmail.com>
2020-01-05 11:13:19 +1000

    Test Checklist

---
+++ commands/test.js
@@ -0,0 +1,50 @@
+//If something needs to know the permissions for this command, it looks here
+exports.permissions = (client) => {
+    return perms = {
+        botChannel: false,           //If true, bot only responds in bot channels
+        adminBotChannel: false,     //If true, bot only responds in admin bot channels
+        role: client.perms.user     //Last word specifies permission level needed to use this command
+    }
+}
+
+//This code is run when the command is executed
+exports.run = (client, message, args) => {
+    const { Client, RichEmbed } = require('discord.js');
+    
+    const sendMessage = `**Commands:**
+AddPlayer
+AddRelic
+Close
+Create
+CreateRelic
+DeleteRelic
+Join
+Leave
+ListRelics
+Load
+MyRelics
+MySquads
+RelicUsers
+RemovePlayer
+RemoveRelic
+Squad
+
+**Features**
+DB removal when player leaves server
+Command restriction (Bot spam stays in bot spam, recruiting stays in recruiting, neither in general chat)
+Permissions (Admin commands for admins only)
+
+**General:**
+Grammar/spelling
+Make sure Help is helpful for every command
+Missing errors - If a command fails (but doesn't crash the bot) it needs to tell you why
+`;
+
+    const embed = new RichEmbed()
+    .setTitle('Things left to test:')
+    .setColor(client.baseConfig.colour)
+    .setDescription(sendMessage);
+
+    message.channel.send(embed);
+
+};
