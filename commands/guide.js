fc60393919cb994e0536d3c42c98550c697bfb72
DarkPhoenix6853 <caleb.klein@hotmail.com>
2020-01-26 19:22:24 +1000

    text update

--- commands/guide.js
+++ commands/guide.js
@@ -12,15 +12,16 @@ exports.run = (client, message, args) => {
     const { Client, RichEmbed } = require('discord.js');
     
     const sendMessage = `Quick overview: 
-- Use __+!addrelic *<relics>*__ in the bot channel to subscribe to relics.
+- Use __+!addrelic *<relics>*__ in the bot channel to subscribe to relics. (Also useful: __+!removerelic__ and __+!listrelics__)
 - Use __+!create *<message>*__ in the recruiting channel to create a host message. Any relics you include will be tagged. Put "1/4" or similar for each squad you wish to create. 
-- If you see a squad you'd like to join, get its ID (the bold number in curly braces after the "1/4") and use __+!join *<ID>*__ in the recruiting channel.
-    
+- If you see a squad you'd like to join, get its ID (the bold number in curly braces after the "1/4") and use __+!join *<ID>*__ in the recruiting channel. (Also useful: __+!leave__ and __+!close__)
+
+- Use __+!help__ for a list of commands, or __+!help *<Command>*__ to see help for a specific command. 
+
 Full guide (with screenshots) is available at: 
 https://github.com/DarkPhoenix6853/PhoenixBot/blob/RelicBot/Guide.pdf 
 
 Alternatively you can:
-- Use ${client.baseConfig.prefix}help
 - Ask a staff member or me (<@198269661320577024>, if I'm online)
 - Check out the Readme on the Github page (More detailed, but less helpful): 
 https://github.com/DarkPhoenix6853/PhoenixBot/blob/RelicBot/README.md`;
