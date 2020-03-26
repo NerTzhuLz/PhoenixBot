58ad666fffc56e6923b068b186b7c90afdf98de9
DarkPhoenix6853 <caleb.klein@hotmail.com>
2020-03-26 21:37:50 +1000

    Even more helpful

--- eventfunctions/message.js
+++ eventfunctions/message.js
@@ -1,17 +1,29 @@
 exports.onMessage = (client, message) => {
     //triggers on every non-bot message
-    if (message.isMemberMentioned(client.user) && (message.content.includes("help")) ) {
-        //if user @'s the bot in a bot channel, or @'s them with "help" in any channel
-        message.reply(`Use __${client.baseConfig.prefix}guide__ or __${client.baseConfig.prefix}help__ in the bot channel to learn how to use this bot.`);
+    if (message.isMemberMentioned(client.user)) {
+        if ((message.content.includes("help")) ) {
+            //if user @'s the bot in a bot channel, or @'s them with "help" in any channel
+            message.reply(`Use __${client.baseConfig.prefix}guide__ or __${client.baseConfig.prefix}help__ in the bot channel to learn how to use this bot.`);
+    
+        } else if ( message.content.includes("core")) {
+            //if a user @'s the bot with "core"
+            message.reply(`Current core: ${client.identity.name} with prefix: ${client.baseConfig.prefix}`);
+    
+        } else if (message.content.includes("prefix")) {
+            //if a user @'s the bot asking for prefix
+            message.reply(`Current prefix: ${client.baseConfig.prefix}`);
 
-    } else if (message.isMemberMentioned(client.user) && message.content.includes("core")) {
-        //if a user @'s the bot with "core"
-        message.reply(`Current core: ${client.identity.name} with prefix: ${client.baseConfig.prefix}`);
+        } else if (message.content.includes("hi")) {
+            //if they're just saying hi
+            message.channel.send(`Hi, ${message.author}`)
 
-    } else if (message.isMemberMentioned(client.user) && message.content.includes("prefix")) {
-        //if a user @'s the bot asking for prefix
-        message.reply(`Current prefix: ${client.baseConfig.prefix}`);
+
+        } else if (botChannels.includes(message.channel.id)) {
+            //if they're @'ing the bot in a bot channel but aren't using any of its predefined responses
+            message.reply(`Use __${client.baseConfig.prefix}guide__ or __${client.baseConfig.prefix}help__ in the bot channel to learn how to use this bot.`);
+        }
     }
+    
 }
 
 exports.commandHandler = (client, message) => {
