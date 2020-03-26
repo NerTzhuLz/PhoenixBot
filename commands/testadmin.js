13f5c1ee0efe72d42fe42e2b43bf83a73ef7c723
DarkPhoenix6853 <caleb.klein@hotmail.com>
2020-03-26 20:25:16 +1000

    Installation tests

---
+++ commands/testadmin.js
@@ -0,0 +1,30 @@
+//If something needs to know the permissions for this command, it looks here
+exports.permissions = (client) => {
+    return perms = {
+        botChannel: true,           //If true, bot only responds in bot channels
+        adminBotChannel: true,     //If true, bot only responds in admin bot channels
+        role: client.perms.admin     //Last word specifies permission level needed to use this command
+    }
+}
+
+//This code is run when the command is executed
+exports.run = (client, message, args) => {
+    //code to run goes here
+    message.channel.send('This should only work if used by an admin, in an elevated bot channel');
+};
+
+//This code is run when "Help" is used to get info about this command
+exports.help = (client, message) => {
+    const { Client, RichEmbed } = require('discord.js');
+    
+    const helpMessage = `(Text here)
+
+Usage: ${client.baseConfig.prefix}CommandName`;
+
+    const embed = new RichEmbed()
+    .setTitle('Help for TEST')
+    .setColor(client.baseConfig.colour)
+    .setDescription(helpMessage);
+
+    message.channel.send(embed);
+};
---
+++ commands/testgeneral.js
@@ -0,0 +1,31 @@
+//If something needs to know the permissions for this command, it looks here
+exports.permissions = (client) => {
+    return perms = {
+        botChannel: false,           //If true, bot only responds in bot channels
+        adminBotChannel: false,     //If true, bot only responds in admin bot channels
+        role: client.perms.trusted     //Last word specifies permission level needed to use this command
+    }
+}
+
+//This code is run when the command is executed
+exports.run = (client, message, args) => {
+    //code to run goes here
+    
+    message.channel.send('This should work in any channel the bot can see');
+};
+
+//This code is run when "Help" is used to get info about this command
+exports.help = (client, message) => {
+    const { Client, RichEmbed } = require('discord.js');
+    
+    const helpMessage = `(Text here)
+
+Usage: ${client.baseConfig.prefix}CommandName`;
+
+    const embed = new RichEmbed()
+    .setTitle('Help for TEST')
+    .setColor(client.baseConfig.colour)
+    .setDescription(helpMessage);
+
+    message.channel.send(embed);
+};
