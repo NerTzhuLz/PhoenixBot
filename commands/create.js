e3d3497cb345871135ed37c93ca7d3bb768475b5
DarkPhoenix6853 <caleb.klein@hotmail.com>
2020-03-27 00:04:34 +1000

    max squad cap set to 300

--- commands/create.js
+++ commands/create.js
@@ -182,7 +182,7 @@ exports.run = (client, message, args) => {
         let lobbyIndex = client.lobbyDB.get('nextLobby');
 
         //set to next index to avoid race conditions
-        if (lobbyIndex == 99) {
+        if (lobbyIndex == 300) {
             client.lobbyDB.set('nextLobby', 0);
         } else {
             client.lobbyDB.set('nextLobby', lobbyIndex + 1);
