ca7ad16ad794b669a8b1d4dea8c6647ee973b81e
DarkPhoenix6853 <caleb.klein@hotmail.com>
2020-03-26 21:50:55 +1000

    guide update

--- commands/guide.js
+++ commands/guide.js
@@ -14,7 +14,7 @@ exports.run = (client, message, args) => {
     const sendMessage = `Quick overview: 
 - Use __${client.baseConfig.prefix}help__ for a list of commands, or __${client.baseConfig.prefix}help *<command>*__ to see help for a specific command. 
 
-- Use __${client.baseConfig.prefix}addrelic *<relics>*__ in the bot channel to subscribe to relics. (Also useful: __${client.baseConfig.prefix}removerelic__ and __${client.baseConfig.prefix}listrelics__)
+- Use __${client.baseConfig.prefix}addrelic *<relics>*__ in the bot channel to subscribe to relics. You can add multiple relics with one command. (Also useful: __${client.baseConfig.prefix}removerelic__ and __${client.baseConfig.prefix}listrelics__)
 - Use __${client.baseConfig.prefix}create *<message>*__ in the recruiting channel to create a host message. Any relics you include will be tagged. Put "1/4" or similar for each squad you wish to create. 
 - If you see a squad you'd like to join, get its ID (the bold number in curly braces after the "1/4") and use __${client.baseConfig.prefix}join *<ID>*__ in the recruiting channel. (Also useful: __${client.baseConfig.prefix}leave__ and __${client.baseConfig.prefix}close__)
 
