e944adfa1e6b1c031e5d319d216994cfd07ede16
DarkPhoenix6853 <caleb.klein@hotmail.com>
2020-05-01 21:30:50 +1000

    perms

--- commands/configreload.js
+++ commands/configreload.js
@@ -1,9 +1,9 @@
 //If something needs to know the permissions for this command, it looks here
 exports.permissions = (client) => {
     return perms = {
-        botChannel: false,           //If true, bot only responds in bot channels
-        adminBotChannel: false,     //If true, bot only responds in admin bot channels
-        role: client.config.get('perms').user     //Last word specifies permission level needed to use this command
+        botChannel: true,           //If true, bot only responds in bot channels
+        adminBotChannel: true,     //If true, bot only responds in admin bot channels
+        role: client.config.get('perms').dev     //Last word specifies permission level needed to use this command
     }
 }
 
