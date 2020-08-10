dadac8274b9a01128556d280e45009f9c7cae7f7
DarkPhoenix6853 <caleb.klein@hotmail.com>
2020-08-10 14:59:27 +1000

    framework funtionality

--- commands/search.js
+++ commands/search.js
@@ -10,22 +10,32 @@ exports.permissions = (client) => {
 //This code is run when the command is executed
 exports.run = (client, message, args) => {
     //Check if argument provided
-    if (args.length == 0) {
-        //Check if relic
-        //Check if vaulted
-        //let results = relicSearch
-        //displayResults
+    if (args.length > 0) {
+        //-----Check if relic (and format correctly)-----
+        //-----Check if vaulted-----
+        let relic = args.join(" ");
+
+        //find open squads with this relic
+        relicSearch(client, relic)
+        .then(output => {
+            //display the result
+            let resultArray = [];
+            resultArray.push(output);
+            displayResults(client, message.channel, resultArray);
+        })
+
     } else {
-        //playerSearch
+        playerSearch(client, message.channel, message.author.id);
     }
 };
 
-let Result = function (message, squadID) {
-    this.message = message;
+let Result = function (messageURL, messageContent, squadID) {
+    this.messageURL = messageURL;
+    this.messageContent = messageContent;
     this.squadID = squadID;
 }
 
-function relicSearch(client, relic) {
+async function relicSearch(client, relic) {
     let results = [];
     //for every squad
     for (let i = 0; i < client.config.get('baseConfig').maxSquads; i++) {
@@ -54,24 +64,53 @@ function relicSearch(client, relic) {
             if (notFound) continue;
 
             //add to results
-            results.push(new Result(squadMessage, currentSquad.lobbyID));
+            results.push(new Result(squadMessage.url, currentSquad.messageContent, currentSquad.lobbyID));
         }
     }
         
-    return {"relic": relic, "results": results};
+    return {"relicName": relic, "results": results};
 }
 
-function playerSearch(client, channel, playerID) {
-    //find each relic the player has
-    //make a result array
-    //for each relic, result = relicSearch
-    //if results, push to result array
+async function playerSearch(client, channel, playerID) {
+    //-----find each relic the player has-----
+
+    let relics = ["Axi A1", "Axi B2", "Axi A3"];
+
+    let resultArray = [];
+    for (relic of relics) {
+        let thisResult = await relicSearch(client, relic);
+        resultArray.push(thisResult);
+    }
+
+    displayResults(client, channel, resultArray);
 }
 
-function displayResults(channel, results) {
+function displayResults(client, channel, results) {
     //passed an array of objects
     //objects have "relic": relic name, "results": array of Result
+
+    let resultMessage = "";
+    for (relic of results) {
+        if (relic.results.length == 0) continue;
+
+        resultMessage += `\n**${relic.relicName}**:\n`;
+        for (result of relic.results) {
+            resultMessage += `[Squad ${result.squadID}](${result.messageURL}): ${result.messageContent}\n`;
+        }
+    }
+
+    if (resultMessage == "") resultMessage = "No open squads found for those relics";
+
+    const { RichEmbed } = require('discord.js');
+
+    const embed = new RichEmbed()
+    .setTitle('Search Results')
+    .setColor(client.config.get('baseConfig').colour)
+    .setDescription(resultMessage);
+
+    channel.send(embed);
 }
+    
 
 //This code is run when "Help" is used to get info about this command
 exports.help = (client, message) => {
